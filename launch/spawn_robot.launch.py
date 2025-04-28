f8b5443aee69377c48225cbb006c8fc0a6bea4f3
Siddarth09 <siddarth.dayasagar@gmail.com>
2025-04-28 19:59:33 -0400

    Fixed JointTrajectoryController

--- launch/spawn_robot.launch.py
+++ launch/spawn_robot.launch.py
@@ -41,7 +41,7 @@ def generate_launch_description():
     spawn_urdf_node = Node(
         package="ros_gz_sim",
         executable="create",
-        arguments=["-name", "husky", "-topic", "robot_description", "-x", "0.0", "-y", "0.0", "-z", "0.9"],
+        arguments=["-name", "husky", "-topic", "robot_description", "-x", "0.0", "-y", "0.0", "-z", "0.2"],
         output="screen",
         parameters=[{'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
@@ -61,23 +61,26 @@ def generate_launch_description():
     )
 
     gz_bridge_node = Node(
-        package="ros_gz_bridge",
-        executable="parameter_bridge",
-        arguments=[
-            "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
-            "/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo",
-            "/imu@sensor_msgs/msg/Imu@gz.msgs.IMU",
-            "/navsat@sensor_msgs/msg/NavSatFix@gz.msgs.NavSat",
-            "/scan@sensor_msgs/msg/LaserScan@gz.msgs.LaserScan",
-            "/scan/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked",
-            "/camera/depth_image@sensor_msgs/msg/Image@gz.msgs.Image",
-            "/camera/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked",
-        ],
-        output="screen",
-        parameters=[
-            {'use_sim_time': LaunchConfiguration('use_sim_time')},
-        ]
-    )
+            package="ros_gz_bridge",
+            executable="parameter_bridge",
+            arguments=[
+                "/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock",
+                "/camera/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo",
+                "/imu@sensor_msgs/msg/Imu@gz.msgs.IMU",
+                "/camera/depth_image@sensor_msgs/msg/Image@gz.msgs.Image",
+                "/camera/depth/points@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked",
+
+                # 🦶 Foot contact sensors:
+                "/lf_foot/contacts@gazebo_msgs/msg/ContactsState[gz.msgs.Contacts",
+                "/lh_foot/contacts@gazebo_msgs/msg/ContactsState[gz.msgs.Contacts",
+                "/rf_foot/contacts@gazebo_msgs/msg/ContactsState[gz.msgs.Contacts",
+                "/rh_foot/contacts@gazebo_msgs/msg/ContactsState[gz.msgs.Contacts",
+            ],
+            output="screen",
+            parameters=[
+                {'use_sim_time': LaunchConfiguration('use_sim_time')},
+            ]
+        )
 
     gz_image_bridge_node = Node(
         package="ros_gz_image",
@@ -116,18 +119,18 @@ def generate_launch_description():
         output='screen'
     )
 
-    # Delay the position controller spawner to avoid early joint interface mismatch
-    position_controller_spawner = TimerAction(
+    joint_trajectory_controller_spawner = TimerAction(
         period=3.0,
         actions=[
             Node(
                 package='controller_manager',
                 executable='spawner',
-                arguments=['effort_controller'],
+                arguments=['joint_trajectory_controller'],
                 output='screen'
             )
         ]
     )
+
 
     static_pose_node= Node(
         package='husky_controllers',
@@ -152,7 +155,7 @@ def generate_launch_description():
     ld.add_action(robot_state_publisher_node)
     ld.add_action(ros2_control_node)
     ld.add_action(joint_state_broadcaster_spawner)
-    ld.add_action(position_controller_spawner)
-    ld.add_action(static_pose_node)
+    ld.add_action(joint_trajectory_controller_spawner)
+    # ld.add_action(static_pose_node)
     
     return ld
